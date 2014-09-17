562366b37785c6f1f107fd2579cf3528800319c1
Hai Dang <dhhai.uns@gmail.com>
2014-09-17 10:54:07 +0700

    Runnable solution

--- database.js
+++ database.js
@@ -1,11 +1,21 @@
-var Sequelize = require('sequelize');
+var Sequelize = require('sequelize')
+	, async = require('async')
+	;
+
+var logger = function(content) {
+	if (database.logStream){
+		database.logStream.write(content);
+		database.logStream.write('\n');	
+	} else {
+		console.log(content);
+	}
+};
 
 var db = new Sequelize(null, null, null, {
 		dialect : 'sqlite',
-		storage : './db.sqlite'
+		storage : './db.sqlite',
+		logging : logger
 });
-
-var limit = 5;
 
 var News = db.define('News', {
 	title : Sequelize.TEXT,
@@ -25,14 +35,43 @@ var Site = db.define('Site', {
 });
 
 Site.hasMany(Category);
-Category.belongsTo(Site);
 Category.hasMany(News);
 
 var database = {
-	"sql" : db,
-	"News" : News,
-	"Category" : Category,
-	"Site" : Site
+	sql : db,
+	News : News,
+	Category : Category,
+	Site : Site,
+	init : function(rssSites, doneInit) {
+		// init db data
+		async.eachSeries(rssSites, function(site, done) {
+			Site.findOrCreate({
+				name : site.name,
+				lastUpdate : new Date(site.last_update)
+		  	}).success(function(newSite, created) {
+
+		  		if (created == true) {
+		  			async.eachSeries(site.rss, function(rssCat, doneRSS) {	
+		  				Category.findOrCreate({
+		  						title : rssCat.category,
+		  						link : rssCat.link
+							}).success(function(catgry) {
+								newSite.addCategory(catgry);
+								doneRSS();
+							});
+
+		  			}, function(err) {
+						if (err)
+							console.log(err);
+						// finally save the site
+						newSite.save();
+					});
+		  		}
+		  		// run each site in parallel
+		  		done();
+			});        
+		}, doneInit);
+	}
 };
 
 module.exports = database;
--- rssextractor.js
+++ rssextractor.js
@@ -6,85 +6,79 @@ var fs = require('fs')
 	;
 
 var limit = 5;
+var interval = 2*3600*1000;
 var rssJSON = './rsslinks.json';
 var rssSites = JSON.parse(fs.readFileSync(rssJSON));
 var parser = new xml2js.Parser();
 
 db.sql.sync().done(function(){
+	async.forever(
+		function(next) {
+			var time = new Date();
 
-	async.eachSeries(rssSites, function(site, done) {
-		db.Site.findOrCreate({
-			name : site.name,
-			lastUpdate : new Date(site.last_update)
-	  	}).success(function(newSite, created){
+			console.log("====RUN====", time);
 
-	  		if (created == true) {
-	  			async.eachSeries(site.rss, function(rssCat, doneRSS) {	
-	  				db.Category.findOrCreate({
-	  						title : rssCat.category,
-	  						link : rssCat.link
-						}).success(function(catgry) {
-							newSite.addCategory(catgry);
-							doneRSS();
-						});
-
-	  			}, function(err) {
-					if (err)
-						console.log(err);
-					// finally save the site
-					newSite.save();
-				});
-	  		}
-	  		// run each site in parallel
-	  		done();
-		});        
-	}, function(err) {
-		if (err)
-			console.log(err);	
-	});
-	
-
-	async.eachSeries(rssSites, function(site, doneSite) {
-		async.eachLimit(site.rss, limit, function(rss, doneRSS) {
-			request(rss.link, function (error, response, body) {
-				if (!error && response.statusCode == 200) {
-					parser.parseString(body, function (err, result) {
-						if (!err) {
-							if (result) {
-								if (result.rss) {
-									var channels = result.rss.channel;
-									if (channels) {
-										for (var k = 0; k < channels.length; k++) {
-											var items = channels[k].item;
-
-											//console.log(items);
-
-											for (var h = 0; h < items.length; h++) {
-												var item = items[h];
-												// console.log(item.title[0], item.pubDate);
-
-												// if (item.pubDate == undefined)
-												// 	console.error("pubDate undefined", rss.link);
+			db.logStream = fs.createWriteStream('test/' + time.toString() + '.log');
+			async.series([
+				function(done) {
+					db.init(rssSites, done);		
+				},
+				function(done) {
+					db.Category
+						.findAll()
+						.success(function(categories) {
+							async.eachLimit(categories, limit, function(rssCat, doneRSS){
+								var update = false;
+								request(rssCat.link, function (error, response, body) {
+									if (!error && response.statusCode == 200 && body) {
+										parser.parseString(body, function (err, result) {
+											if (!err && result && result.rss) {
+												async.each(result.rss.channel, function(channel, doneChannel){
+													async.each(channel.item, function(item, doneItem) {	
+										  				db.News.findOrCreate({
+									  						title : item.title[0],
+									  						link : item.link ? item.link[0] : null
+															}, {
+																title : item.title[0],
+																link : item.link ? item.link[0] : null,
+																pubDate : item.pubDate ? new Date(item.pubDate[0]) : null,
+																guid : item.guid ? item.guid[0] : null
+															}).success(function(news, created) {
+																if (created) {
+																	rssCat.addNew(news);
+																	update = true;
+																}
+																doneItem();
+															}).error(doneItem);
+										  			}, doneChannel);
+												}, function(err) {
+													// finally save the foreign keys
+													if (update) {
+														rssCat.save().done(doneRSS);
+													} else {
+														doneRSS();
+													}
+												});
+											} else {
+												doneRSS(error);
 											}
-										}
+										});
+									} else {
+										doneRSS(error);
 									}
-								}
-							} else {
-								console.error("null result" , rss.link, body);
-							}
-							doneRSS();
-						} else {
-							doneRSS(err);
-						}
-				    });
-				} else {
-					doneRSS(error);
+								});	
+						}, done);
+					}).error(done);
 				}
+			], function (err, results) { 
+				db.logStream.end();
+				console.log("====DONE====", new Date());
+				setTimeout(next, interval);
 			});
-		}, doneSite);
-	}, function(err) {
-		if (err)
-			console.log(err);	
-	});
+		},
+		function (err) {
+			console.log("====FOREVER_ERROR====", err);
+		}
+	);
 });
 
